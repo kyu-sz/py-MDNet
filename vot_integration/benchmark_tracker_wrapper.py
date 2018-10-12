ccd7a094296ce72dd8a80b5caeaadb64d58ecc22
Ken Yu <cn.ken.yu@gmail.com>
2018-10-12 13:51:48 -0400

    Add debug message for the VOT wrapper.

--- vot_integration/benchmark_tracker_wrapper.py
+++ vot_integration/benchmark_tracker_wrapper.py
@@ -12,25 +12,34 @@ tracking_module_path = os.path.join(script_dir, '../tracking')
 sys.path.insert(0, tracking_module_path)
 from tracker import Tracker
 
+print('Creating VOT handle...')
 handle = vot.VOT("rectangle")
+print('Retrieving the target selection...')
 selection = handle.region()
 
 # Process the first frame
+print('Retrieving the first frame...')
 imagefile = handle.frame()
 if not imagefile:
+    print('Cannot retrieve the first frame!')
     sys.exit(0)
 first_frame = Image.open(imagefile).convert('RGB')
 
+print('Initializing tracker...')
 mdnet = Tracker((selection.x, selection.y, selection.width, selection.height),
                 first_frame,
-                gpu=0)
+                gpu=1)
 
 while True:
+    print('Retrieving a new frame...')
     imagefile = handle.frame()
     if not imagefile:
+        print('Cannot retrieve a new frame!')
         break
     frame = Image.open(imagefile).convert('RGB')
 
     pred_bbox, confidence = mdnet.track(frame)
 
     handle.report(Rectangle(pred_bbox[0], pred_bbox[1], pred_bbox[2], pred_bbox[3]), confidence)
+
+print('Exiting...')
